e4a6bfae736bc0a70de5387422dc8a7f4b37fffe
DettyJu <rama.363597@pasay-edsa.sti.edu.ph>
2025-01-24 21:46:02 +0800

    Add files via upload

---
+++ Prelim/Laboratories/01 hands-on act 1/GreetingApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingApp
+{
+    internal class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int courses;
+            decimal price;
+
+            Console.Write("Enter Your Name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter the total number of your enrolled courses : ");
+            courses = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the price of your favorite book: ");
+            price = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("\nName : " + name);
+            Console.WriteLine("Total enrolled courses: " + courses);
+            Console.WriteLine("Price of my favorite book: " + price);
+
+            Console.Write("\nPress any key to exit... ");
+            Console.ReadKey();
+
+
+        }
+    }
+}
